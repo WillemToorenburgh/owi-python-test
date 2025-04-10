2b53a347480890dec0c30df5b3b9ce4b663627ee
Willem Toorenburgh <willemtoorenburgh@gmail.com>
2025-04-10 16:29:21 -0700

    Commit supporting files

---
+++ shell.nix
@@ -0,0 +1,13 @@
+with import <nixpkgs> {}; let
+  pythonEnv = python3.withPackages (ps:
+    with ps; [
+      google-api-python-client
+      google-auth-httplib2
+      google-auth-oauthlib
+      pytest
+      pylance
+      debugpy
+      pylint
+    ]);
+in
+  pythonEnv.env
